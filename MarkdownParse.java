138fbcfa77f050abc934f1445c44203eedd3431a
cindy1851646 <cindy.jw.chen@gmail.com>
2022-02-16 04:28:49 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -47,7 +47,6 @@ public class MarkdownParse {
 //git commit -m mdparse
 */
 
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
